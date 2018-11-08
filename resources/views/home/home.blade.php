4d987080a9b142f8f92708b0cab5b98b41f56f74
Vallin Plasencia <vallin.plasencia@gmail.com>
2018-11-08 14:04:49 -0500

    Agregando parte del FrontEnd para el CRUD de categoria y proceso

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -7,19 +7,14 @@
 
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 
-    <link rel="stylesheet" href="styles.d3d47e79093453d62780.css">
-</head>
+    <link rel="stylesheet" href="styles.d3d47e79093453d62780.css"></head>
 <body>
 <app-root></app-root>
-<script type="text/javascript" src="runtime.6e933b95853425892893.js"></script>
-<script type="text/javascript" src="polyfills.c6871e56cb80756a5498.js"></script>
-<script type="text/javascript" src="main.03e2a222f1a36bdeecff.js"></script>
-</body>
+<script type="text/javascript" src="runtime.c322f978b4c33b205903.js"></script><script type="text/javascript" src="polyfills.c6871e56cb80756a5498.js"></script><script type="text/javascript" src="main.f24a48797871ad6051fa.js"></script></body>
 </html>
